eea96ce56f45d431e9c4d2e93869708e30ac8586
ozgeyaylagul <103510452+ozgeyaylagul@users.noreply.github.com>
2023-02-13 03:19:45 +0200

    Add props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import { Course } from './Course';
+import Course from './Course';
 
 function App() {
   return (
     <div className="App">
-      <Course/>
+      <Course title="Angular" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course title="Bootstrap" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course title="JavaScript" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course title="React" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export const Course = () => {
-  return <div>Course</div>;
+function Course({title, description}) {
+    // console.log(props);
+    // const title= props.title;
+    // const description=props.description;
+    // const {title, description} = props;
+  return (
+    <div>
+        <div>{title}</div>;
+        <div>{description}</div>;
+    </div>
+  );
 }
+
+export default Course;
