03f01bf975b67ed39affe22a903051134308251f
ozgeyaylagul <103510452+ozgeyaylagul@users.noreply.github.com>
2023-02-14 03:31:48 +0200

    Use Bulma

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import 'bulma/css/bulma.css';
 import './App.css';
 import Course from './Course';
 import Angular from "./images/angular.jpg"
 import Bootstrap from "./images/bootstrap5.png"
 import Csharp from "./images/ccsharp.png"
-import KompleWeb from "./images/kompleweb.jpg"
+import Frontend from "./images/kompleweb.jpg"
 
 function App() {
   return (
     <div className="App">
-      <Course image={Angular}
-       title="Angular" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course image={Bootstrap} title="Bootstrap 5" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course image={KompleWeb} title="Komple Web" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course image={Csharp} title="C Sharp" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <section className="hero is-link">
+  <div className="hero-body">
+    <p className="title">
+      My Courses
+    </p>
+  </div>
+</section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+          <Course image={Angular}title="Angular" description="Lorem ipsum dolor sit amet. Et impedit quia ad dolores nobis quo dolor modi sit adipisci harum! Non tempora totam et voluptatem quia est nobis quaerat aut aliquam dolor in autem molestias hic minima voluptatibus. Ut dolores cupiditate et Quis aliquid sed beatae voluptas. Est Quis rerum et internos neque non harum vitae et dolores rerum aut tempora velit."/>
+          </div>
+          <div className='column'>
+          <Course image={Bootstrap} title="Bootstrap 5" description="Lorem ipsum dolor sit amet. Et impedit quia ad dolores nobis quo dolor modi sit adipisci harum! Non tempora totam et voluptatem quia est nobis quaerat aut aliquam dolor in autem molestias hic minima voluptatibus. Ut dolores cupiditate et Quis aliquid sed beatae voluptas. Est Quis rerum et internos neque non harum vitae et dolores rerum aut tempora velit."/>
+          </div>
+          <div className='column'>
+          <Course image={Frontend} title="Frontend" description="Lorem ipsum dolor sit amet. Et impedit quia ad dolores nobis quo dolor modi sit adipisci harum! Non tempora totam et voluptatem quia est nobis quaerat aut aliquam dolor in autem molestias hic minima voluptatibus. Ut dolores cupiditate et Quis aliquid sed beatae voluptas. Est Quis rerum et internos neque non harum vitae et dolores rerum aut tempora velit."/>
+          </div>
+          <div className='column'>
+          <Course image={Csharp} title="C Sharp" description="Lorem ipsum dolor sit amet. Et impedit quia ad dolores nobis quo dolor modi sit adipisci harum! Non tempora totam et voluptatem quia est nobis quaerat aut aliquam dolor in autem molestias hic minima voluptatibus. Ut dolores cupiditate et Quis aliquid sed beatae voluptas. Est Quis rerum et internos neque non harum vitae et dolores rerum aut tempora velit."/>
+          </div>
+        </div>
+        </section>
+      </div>
     </div>
   );
 }
