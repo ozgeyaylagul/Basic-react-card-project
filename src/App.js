d4921496194b72ab5824cda60d18099da107b499
ozgeyaylagul <103510452+ozgeyaylagul@users.noreply.github.com>
2023-02-14 02:43:16 +0200

    Send photos as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Course from './Course';
+import Angular from "./images/angular.jpg"
+import Bootstrap from "./images/bootstrap5.png"
+import Csharp from "./images/ccsharp.png"
+import KompleWeb from "./images/kompleweb.jpg"
 
 function App() {
   return (
     <div className="App">
-      <Course title="Angular" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course title="Bootstrap" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course title="JavaScript" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
-      <Course title="React" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course image={Angular}
+       title="Angular" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course image={Bootstrap} title="Bootstrap 5" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course image={KompleWeb} title="Komple Web" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
+      <Course image={Csharp} title="C Sharp" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, tempore cumque"/>
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Course({title, description}) {
+function Course({image, title, description}) {
     // console.log(props);
     // const title= props.title;
     // const description=props.description;
     // const {title, description} = props;
   return (
     <div>
+      <img src={image} alt="" />
         <div>{title}</div>;
         <div>{description}</div>;
     </div>
